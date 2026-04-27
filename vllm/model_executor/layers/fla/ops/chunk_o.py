acd8356f76b5557780b94412bc5c6c4568398f77
adam.wang <604583565@qq.com>
2026-04-27 15:07:52 +0800

    feat(op): add op release.

--- vllm/model_executor/layers/fla/ops/chunk_o.py
+++ vllm/model_executor/layers/fla/ops/chunk_o.py
@@ -111,8 +111,9 @@ def chunk_fwd_kernel_o(
 
     if USE_G:
         g += bos * H + i_h
-        p_g = tl.make_block_ptr(g, (T,), (H,), (i_t * BT,), (BT,), (0,))
-        b_g = tl.load(p_g, boundary_check=(0,))
+        offs_t = i_t * BT + tl.arange(0, BT)
+        ptrs = g + offs_t * H
+        b_g = tl.load(ptrs, mask=offs_t < T, other=0)
         b_o = b_o * exp(b_g)[:, None]
         b_A = b_A * exp(b_g[:, None] - b_g[None, :])
 
